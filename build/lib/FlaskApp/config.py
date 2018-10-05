ed9c848fc2cfb2494a9ef66c5f9c0c8534651a9d
wfmonster <wireframemonster@gmail.com>
2018-10-05 17:22:42 -0500

    fixed untracked files

--- build/lib/FlaskApp/config.py
+++
@@ -1,7 +0,0 @@
-DEBUG = True # Turns on debugging features in Flask
-BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
-MAIL_FROM_EMAIL = "wireframemonster@gmail.com" # For use in application emails
-
-SECRET_KEY = 'some_super_long_secret_key'
-ADMINS = ['beck@uriolabs.com']
-
